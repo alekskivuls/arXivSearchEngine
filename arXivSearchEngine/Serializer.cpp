d951ddd76a644b851f8b1091adeb525d1729ed86
Aleks Kivuls <akivuls@yahoo.com>
2016-11-14 03:32:20 -0800

    Fix double support

--- arXivSearchEngine/Serializer.cpp
+++ arXivSearchEngine/Serializer.cpp
@@ -9,7 +9,7 @@ inline uint32_t Reverse(uint32_t value) {
 		(value & 0x000000FF) << 24;
 }
 
-/*inline uint32_t Reverse(uint32_t value) {
+inline uint64_t Reverse(uint64_t value) {
 	return (value & 0xFF00000000000000) >> 56 |
 		(value & 0x00FF000000000000) >> 40 |
 		(value & 0x0000FF0000000000) >> 24 |
@@ -18,7 +18,7 @@ inline uint32_t Reverse(uint32_t value) {
 		(value & 0x0000000000FF0000) << 24 |
 		(value & 0x000000000000FF00) << 40 |
 		(value & 0x00000000000000FF) << 56;
-}*/
+}
 
 inline double_t Reverse(double_t value) {
 	auto result = Reverse(*reinterpret_cast<uint32_t*>(&value));
