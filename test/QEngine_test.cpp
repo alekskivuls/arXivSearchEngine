a0e3e08f8208e9d7d5f3b8f43d848bd42326a638
Aleks Kivuls <akivuls@yahoo.com>
2016-11-25 14:02:05 -0800

    Create query engine testing skeleton

---
+++ test/QEngine_test.cpp
@@ -0,0 +1,39 @@
+#include "QEngine.h"
+#include "gtest/gtest.h"
+
+class QEngineTest : public ::testing::Test {
+ protected:
+  //virtual void SetUp() {}
+  //virtual void TearDown() {}
+  QEngine queryEngine;
+};
+
+TEST_F(QEngineTest, regularQueries)
+{
+
+}
+
+TEST_F(QEngineTest, andQueries)
+{
+
+}
+
+TEST_F(QEngineTest, orQueries)
+{
+
+}
+
+TEST_F(QEngineTest, notQueries)
+{
+
+}
+
+TEST_F(QEngineTest, phraseQueries)
+{
+
+}
+
+TEST_F(QEngineTest, rankQueries)
+{
+
+}
