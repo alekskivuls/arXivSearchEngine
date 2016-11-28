6a877c2e1393f70e83bd7cf9612cf3be00099ddc
Aleks Kivuls <akivuls@yahoo.com>
2016-11-27 23:55:30 -0800

    Remove colons from DocInfo documentation

--- arXivSearchEngine/DocInfo.h
+++ arXivSearchEngine/DocInfo.h
@@ -6,37 +6,37 @@
 #include <list>
 
 /**
- * @brief The DocInfo class : The document info represents a single document in a single postings list.
+ * @brief The DocInfo class The document info represents a single document in a single postings list.
  * This Object contains all positional instances/occurences of "term" that
  * this "document" is associated with.
  */
 class DocInfo {
-    /** @brief _docId : The document name that is associated with the stemmed term in the index.*/
+    /** @brief _docId The document name that is associated with the stemmed term in the index.*/
     uint32_t _docId;
 
-    /** @brief _posList : A list of positional occurences of the term associated with the document.*/
+    /** @brief _posList A list of positional occurences of the term associated with the document.*/
     std::list<uint32_t> _posList;
 
 public:
-    /** @brief DocInfo : Create an empty postings list for a document
-     *  @param docId : The document id to create an empty postings list for
+    /** @brief DocInfo Create an empty postings list for a document
+     *  @param docId The document id to create an empty postings list for
      */
     DocInfo(const uint32_t &docId);
 
     /**
-     * @brief addPosition : Adds a position to the end of the document's posting list (not inserted)
-     * @param pos : The position to add
+     * @brief addPosition Adds a position to the end of the document's posting list (not inserted)
+     * @param pos The position to add
      */
     void addPosition(const uint32_t &pos);
 
     /**
-     * @brief getDocId : Gets the name of the document (json file name).
+     * @brief getDocId Gets the name of the document (json file name).
      * @return The document id number.
      */
 	uint32_t getDocId() const;
 
     /**
-     * @brief getPositions : Gets the list of the term's occurence.
+     * @brief getPositions Gets the list of the term's occurence.
      * @return The list of occurences.
      */
     std::list<uint32_t> getPositions() const;
