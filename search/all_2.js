afc1f9fce53232798b174a2e81721149bc2ca09c
Documentation Builder <travis@travis-ci.org>
2016-12-08 19:59:54 +0000

    Automated documentation build for changeset 0b72358daa09486b22251a4e6df5094739cd9f5b.

--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['classifierclass',['ClassifierClass',['../classClassifierClass.html',1,'']]],
   ['classifierengine',['ClassifierEngine',['../classClassifierEngine.html',1,'']]],
   ['compressor',['Compressor',['../classCompressor.html',1,'']]],
   ['correctspelling',['correctSpelling',['../classKEngine.html#ae31417ec11fa03414dcde069699bd2f5',1,'KEngine']]]
--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['classifierclass',['ClassifierClass',['../classClassifierClass.html',1,'']]],
   ['classifierengine',['ClassifierEngine',['../classClassifierEngine.html',1,'']]],
   ['compressor',['Compressor',['../classCompressor.html',1,'']]]
 ];
