78c38549b81587890f5040cd22e39c6b8803a721
Documentation Builder <travis@travis-ci.org>
2016-12-09 02:57:14 +0000

    Automated documentation build for changeset 5949e23b6df85709718b01cdc254fea71c8d10eb.

--- search/all_3.js
+++ search/all_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['descending',['descending',['../structQEngine_1_1descending.html',1,'QEngine']]],
   ['diskinvertedindex',['DiskInvertedIndex',['../classDiskInvertedIndex.html',1,'']]],
+  ['diskio',['DiskIo',['../classDiskIo.html',1,'']]],
   ['docinfo',['DocInfo',['../classDocInfo.html',1,'DocInfo'],['../classDocInfo.html#ae768e3b909f33d9d3b3fedce90c2c30a',1,'DocInfo::DocInfo()']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['descending',['descending',['../structQEngine_1_1descending.html',1,'QEngine']]],
   ['diskinvertedindex',['DiskInvertedIndex',['../classDiskInvertedIndex.html',1,'']]],
+  ['diskio',['DiskIo',['../classDiskIo.html',1,'']]],
   ['docinfo',['DocInfo',['../classDocInfo.html',1,'']]]
 ];
